3ed21e68883f3905839722ac6858409265baf58e
unknown <m.r.fouladi7@gmail.com>
2023-12-19 09:37:48 +0330

    fix ball position for all players

--- javascripts/script.js
+++ javascripts/script.js
@@ -90,6 +90,12 @@ function ballReset() {
   ballX = width / 2;
   ballY = height / 2;
   speedY = 3;
+
+  socket.emit("ballMove", {
+    ballX,
+    ballY,
+    score,
+  });
 }
 
 // Adjust Ball Movement
@@ -100,6 +106,12 @@ function ballMove() {
   if (playerMoved) {
     ballX += speedX;
   }
+
+  socket.emit("ballMove", {
+    ballX,
+    ballY,
+    score,
+  });
 }
 
 // Determine What Ball Bounces Off, Score Points, Reset Ball
@@ -155,9 +167,13 @@ function ballBoundaries() {
 
 // Called Every Frame
 function animate() {
-  ballMove();
+  if (isReferee) {
+    ballMove();
+    ballBoundaries();
+  }
+
   renderCanvas();
-  ballBoundaries();
+
   window.requestAnimationFrame(animate);
 }
 
@@ -207,4 +223,8 @@ socket.on("paddleMove", (paddleData) => {
   //Toggle 1 to 0, and 0 to 1
   const opponentPaddleIndex = 1 - paddleIndex;
   paddleX[opponentPaddleIndex] = paddleData.xPosition;
+});
+
+socket.on("ballMove", (ballData) => {
+  ({ ballX, ballY, score } = ballData);
 });
--- server.js
+++ server.js
@@ -21,6 +21,10 @@ io.on("connection", (socket) => {
   socket.on("paddleMove", (paddleData) => {
     socket.broadcast.emit("paddleMove", paddleData);
   });
+
+  socket.on("ballMove", (ballData) => {
+    socket.broadcast.emit("ballMove", ballData);
+  });
 });
 
 server.listen(PORT, () => {
