0279f1cd62fea28ec0a99a879270162864fef603
unknown <m.r.fouladi7@gmail.com>
2023-12-19 09:20:48 +0330

    fix paddle position for all players

--- javascripts/script.js
+++ javascripts/script.js
@@ -2,6 +2,7 @@
 const canvas = document.createElement("canvas");
 const context = canvas.getContext("2d");
 const socket = io("http://localhost:3000");
+let isReferee = false;
 let paddleIndex = 0;
 
 let width = 500;
@@ -160,13 +161,14 @@ function animate() {
   window.requestAnimationFrame(animate);
 }
 
-// Start Game, Reset Everything
-function startGame() {
+// Load Game, Reset Everything
+function loadGame() {
   createCanvas();
   renderIntro();
   socket.emit("ready");
-
-  paddleIndex = 0;
+}
+function startGame() {
+  paddleIndex = isReferee ? 0 : 1;
   window.requestAnimationFrame(animate);
   canvas.addEventListener("mousemove", (e) => {
     playerMoved = true;
@@ -177,14 +179,32 @@ function startGame() {
     if (paddleX[paddleIndex] > width - paddleWidth) {
       paddleX[paddleIndex] = width - paddleWidth;
     }
+
+    socket.emit("paddleMove", {
+      xPosition: paddleX[paddleIndex],
+    });
     // Hide Cursor
     canvas.style.cursor = "none";
   });
 }
 
 // On Load
-startGame();
+loadGame();
 
 socket.on("connect", () => {
   console.log(`Connected as ${socket.id}`);
+});
+
+socket.on("startGame", (refereeId) => {
+  console.log(`Referee is ${refereeId}`);
+
+  isReferee = socket.id === refereeId;
+
+  loadGame();
+});
+
+socket.on("paddleMove", (paddleData) => {
+  //Toggle 1 to 0, and 0 to 1
+  const opponentPaddleIndex = 1 - paddleIndex;
+  paddleX[opponentPaddleIndex] = paddleData.xPosition;
 });
--- server.js
+++ server.js
@@ -3,8 +3,24 @@ const io = require("socket.io")(server, { origins: "*:*" });
 
 const PORT = 3000;
 
+let readyPlayerCount = 0;
+
 io.on("connection", (socket) => {
   console.log("User connected as " + socket.id);
+
+  socket.on("ready", () => {
+    console.log("Player ready", socket.id);
+    console.log(":::::::::::::::::::::::::::::::");
+    readyPlayerCount++;
+
+    if (readyPlayerCount === 2) {
+      io.emit("startGame", socket.id);
+    }
+  });
+
+  socket.on("paddleMove", (paddleData) => {
+    socket.broadcast.emit("paddleMove", paddleData);
+  });
 });
 
 server.listen(PORT, () => {
